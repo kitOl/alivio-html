b43b2742e8e678b2ff3947f2f3254b3938c6fe6c
Oleg Kitaev <kitaev_oleg@mail.ru>
2024-02-21 09:33:19 +0300

    add video in story

--- js/main.js
+++ js/main.js
@@ -7,3 +7,33 @@ mobileNavButton.addEventListener("click", function () {
   mobileNav.classList.toggle("active");
   document.body.classList.toggle("no-scroll");
 });
+
+const videoBtn = document.querySelector("#video-story-btn");
+const videoBtnIcon = document.querySelector("#video-story-btn-icon");
+const videoOverlay = document.querySelector("#video-story-overlay");
+const videoFile = document.querySelector("#video-story");
+
+videoBtn.addEventListener("click", function () {
+  function toggleOverlay(event) {
+    if (event.type === "mouseleave") {
+      videoOverlay.classList.add("hidden");
+    } else {
+      videoOverlay.classList.remove("hidden");
+    }
+  }
+
+  if (videoFile.paused) {
+    videoFile.play();
+    videoBtnIcon.src = "./img/story/pause-btn.svg";
+    videoOverlay.classList.add("hidden");
+
+    videoOverlay.onmouseleave = toggleOverlay;
+    videoOverlay.onmouseenter = toggleOverlay;
+  } else {
+    videoFile.pause();
+    videoBtnIcon.src = "./img/story/play-btn.svg";
+    videoOverlay.classList.remove("hidden");
+
+    videoOverlay.onmouseleave = null;
+  }
+});
